774ed5798c1c5c5ced2b55afd3698399343ad3f2
Victoria Bruno <paula.victoria.bruno@gmail.com>
2023-05-21 15:02:06 -0300

    Cambios formulario

--- js/contac.js
+++ js/contac.js
@@ -1,21 +1,22 @@
-
 var nombre = document.getElementById("form-forename");
 var apellido = document.getElementById("form-surname");
 var email = document.getElementById("form-email");
 var validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 var seleccion = document.getElementById("ckeck");
+var formulario = document.getElementById("formulario");
 
 function validarFormulario() {
-   console.log(nombre);
    var mensajeError = [];
    let infoCompleta = true;
 
-   if (nombre.value === null || nombre.value === "") {
-      mensajeError.push("Debe ingresar su apellido");
+   
+   if (nombre.value === "" || /^\s+$/.test(nombre.value)) {
+      mensajeError.push("Debe ingresar un nombre válido");
       infoCompleta = false;
    }
-   if (apellido.value === null || apellido.value === "") {
-      mensajeError.push("Debe ingresar su apellido");
+
+   if (apellido.value === "" || /^\s+$/.test(nombre.value) ) {
+      mensajeError.push("Debe ingresar un apellido");
       infoCompleta = false;
 
    }
@@ -26,12 +27,11 @@ function validarFormulario() {
 
    }
 
-
    if (intereses.selectedIndex == 0) {
-      mensajeError.push("Debe seleccionar en que servicio esta interesado.");
+      mensajeError.push("Debe seleccionar el servicio por el cual esta interesado.");
       infoCompleta = false;
    }
-
+   
    if (infoCompleta === false) {
       Swal.fire({
          text: mensajeError.join(". "),
@@ -40,14 +40,17 @@ function validarFormulario() {
 
    }
 
-   if (infoCompleta === true) {
+   if (infoCompleta === true){
       Swal.fire({
          title: "Muchas gracias",
          text: "Lo estaremos contactando a la brevedad",
          icon: "success",
+   
       });
+      {
+         formulario.reset();
+      }
    }
-
 }
 
 
